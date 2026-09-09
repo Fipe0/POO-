352bec6cca867774d149ab6a00eb376ca3cc0484
Fipe L R <lugonesrochafilipe@gmail.com>
2026-09-09 13:57:11 -0300

    Melhorei o program

--- Program.cs
+++ Program.cs
@@ -2,19 +2,30 @@ Animal cachorro = new Animal("Martelo", 5, "Cachorro");
 Animal hamster = new Animal("Jorge", 5, "Hamster");
 
 cachorro.Comer();
+Console.WriteLine();
+
 cachorro.Dormir();
+Console.WriteLine();
+
 cachorro.Som();
+Console.WriteLine();
 
 Console.WriteLine(cachorro);
 
+Console.WriteLine("\n====================\n");
 
 hamster.Comer();
+Console.WriteLine();
+
 hamster.Dormir();
+Console.WriteLine();
+
 hamster.Som();
+Console.WriteLine();
 
 Console.WriteLine(hamster);
 
-
+Console.WriteLine("\n====================\n");
 
 Celular pessoal = new Celular("Samsung", "Galaxy S24", 50);
 Celular corporativo = new Celular("Motorola", "Edge 50 Pro", 75);
@@ -25,6 +36,7 @@ pessoal.Ligar();
 pessoal.Carregar();
 pessoal.Desligar();
 
+Console.WriteLine();
 
 Console.WriteLine(corporativo);
 
@@ -32,7 +44,7 @@ corporativo.Ligar();
 corporativo.Carregar();
 corporativo.Desligar();
 
-
+Console.WriteLine("\n====================\n");
 
 Player jogador1 = new Player("Filipe", 1, 100);
 Player jogador2 = new Player("Josefa", 1, 100);
@@ -42,6 +54,8 @@ jogador1.Defender();
 jogador1.SubirNivel();
 
 Console.WriteLine(jogador1);
+
+Console.WriteLine();
 
 jogador2.Atacar();
 jogador2.Defender();
