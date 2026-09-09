62b8a3ad6e76bd49d1ea1ee9e6760f5605a7bb22
Fipe L R <lugonesrochafilipe@gmail.com>
2026-09-09 13:42:29 -0300

    Add new instances of Animal, Celular, and Player

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 Animal cachorro = new Animal("Martelo", 5, "Cachorro");
+Animal hamster = new Animal("Jorge", 5, "Hamster");
 
 cachorro.Comer();
 cachorro.Dormir();
@@ -7,20 +8,43 @@ cachorro.Som();
 Console.WriteLine(cachorro);
 
 
+hamster.Comer();
+hamster.Dormir();
+hamster.Som();
 
-Celular celular = new Celular("Samsung", "Galaxy S24", 50);
-
-Console.WriteLine(celular);
-
-celular.Ligar();
-celular.Carregar();
-celular.Desligar();
+Console.WriteLine(hamster);
 
 
-Player jogador = new Player("Filipe", 1, 100);
 
-jogador.Atacar();
-jogador.Defender();
-jogador.SubirNivel();
+Celular pessoal = new Celular("Samsung", "Galaxy S24", 50);
+Celular corporativo = new Celular("Motorola", "Edge 50 Pro", 75);
 
-Console.WriteLine(jogador);
+Console.WriteLine(pessoal);
+
+pessoal.Ligar();
+pessoal.Carregar();
+pessoal.Desligar();
+
+
+Console.WriteLine(corporativo);
+
+corporativo.Ligar();
+corporativo.Carregar();
+corporativo.Desligar();
+
+
+
+Player jogador1 = new Player("Filipe", 1, 100);
+Player jogador2 = new Player("Josefa", 1, 100);
+
+jogador1.Atacar();
+jogador1.Defender();
+jogador1.SubirNivel();
+
+Console.WriteLine(jogador1);
+
+jogador2.Atacar();
+jogador2.Defender();
+jogador2.SubirNivel();
+
+Console.WriteLine(jogador2);
