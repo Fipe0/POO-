23a100723254fcb82d3a92774b932938cb23aa66
Fipe L R <lugonesrochafilipe@gmail.com>
2026-09-09 13:33:11 -0300

    Player program

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ cachorro.Comer();
 cachorro.Dormir();
 cachorro.Som();
 
-
 Console.WriteLine(cachorro);
 
 
@@ -18,12 +17,10 @@ celular.Carregar();
 celular.Desligar();
 
 
-ContaBancaria conta1 = new ContaBancaria("Carlos", 1001, 1500.00);
-ContaBancaria conta2 = new ContaBancaria("Marcos", 1002, 2500.00);
+Player jogador = new Player("Filipe", 1, 100);
 
-Console.WriteLine("\n" + conta1);
-Console.WriteLine(conta2);
+jogador.Atacar();
+jogador.Defender();
+jogador.SubirNivel();
 
-conta1.Saldo = 2000.00;
-Console.WriteLine("\nDepois de alterar o saldo:");
-Console.WriteLine(conta1);
+Console.WriteLine(jogador);
